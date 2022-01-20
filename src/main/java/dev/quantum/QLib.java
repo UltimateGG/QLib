225125fa1d887ded6e87db71abcd98e6a9b7f517
Gamer745 <clubblock1@gmail.com>
2022-01-19 21:00:04 -0600

    Initial Commit

---
+++ src/main/java/dev/quantum/QLib.java
@@ -0,0 +1,5 @@
+package dev.quantum;
+
+public class QLib {
+    public static final boolean DEVELOPMENT_MODE = Boolean.getBoolean("dev.quantum.devmode");
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,7 @@
+import dev.quantum.QLib;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test " + QLib.DEVELOPMENT_MODE);
+    }
+}
